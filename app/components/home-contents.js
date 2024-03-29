7485bf0d350926b48c5a34ce742ba5c7d7474e67
Sarah <snp.orbenet@gmail.com>
2024-03-29 00:09:55 -0600

    Conditional rendering for home page

--- app/components/home-contents.js
+++ app/components/home-contents.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react"
+
 export default function HomeContent(){
     const styles = {
         background1: {
@@ -8,33 +12,75 @@ export default function HomeContent(){
         }
       }
 
+    const[headerName, setHeaderName] = useState('Overview');
+    const[clicked, setClicked] = useState('Overview');
+
     return(
         <div className="w-3/4 ml-auto mr-auto">
             <div>
                 <div className="mt-28">
-                    <div className="text-xl text-center pt-2 pb-2 flex flex-row w-full" style={styles.background1}>
-                        <div className="w-1/5">
-                            Summer
+                    <div className="text-xl text-center pt-2 pb-2 flex flex-row w-full" style={styles.background2}>
+                        <div className="w-1/5" onClick={() => {setHeaderName('Summer'); setClicked('Summer')}}>
+                            {clicked == 'Summer' ? (
+                                <div className="bg-yellow-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Summer</div>) :
+                                (<div className="bg-gray-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Summer</div>)
+                            }
                         </div>
-                        <div className="w-1/5">
-                            Spring
+                        <div className="w-1/5" onClick={() => {setHeaderName('Spring'); setClicked('Spring')}}>
+                            {clicked == 'Spring' ? (
+                                <div className="bg-pink-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Spring</div>) :
+                                (<div className="bg-gray-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Spring</div>)
+                            }
                         </div>
-                        <div className="w-1/5">
-                            Overview
+                        <div className="w-1/5" onClick={() => {setHeaderName('Overview'); setClicked('Overview')}}>
+                            {clicked == 'Overview' ? (
+                                <div className="bg-red-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Overview</div>) :
+                                (<div className="bg-gray-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Overview</div>)
+                            }
                         </div>
-                        <div className="w-1/5">
-                            Fall
+                        <div className="w-1/5" onClick={() => {setHeaderName('Fall'); setClicked('Fall')}}>
+                            {clicked == 'Fall' ? (
+                                <div className="bg-orange-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Fall</div>) :
+                                (<div className="bg-gray-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Fall</div>)
+                            }
                         </div>
-                        <div className="w-1/5">
-                            Winter
+                        <div className="w-1/5" onClick={() => {setHeaderName('Winter'); setClicked('Winter')}}>
+                            {clicked == 'Winter' ? (
+                                <div className="bg-blue-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Winter</div>) :
+                                (<div className="bg-gray-300 rounded-xl border-green-900 border-2 w-44 ml-auto mr-auto cursor-pointer">Winter</div>)
+                            }
                         </div>
                     </div>
                     <div className=" text-lg pl-10 h-80" style={styles.background1}>
-                        This is the Overview.
+                        {headerName == 'Summer' && (
+                            <div>
+                                Summer
+                            </div>
+                        )}
+                        {headerName == 'Spring' && (
+                            <div>
+                                Spring
+                            </div>
+                        )}
+                        {headerName == 'Overview' && (
+                            <div>
+                                Overview
+                            </div>
+                        )}
+                        {headerName == 'Fall' && (
+                            <div>
+                                Fall
+                            </div>
+                        )}
+                        {headerName == 'Winter' && (
+                            <div>
+                                Winter
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div className="border-2 border-green-500 mt-20">
-                    <div className="text-4xl text-center pt-10 pb-10" style={styles.background1}>
+                <div className="mt-20">
+                    <div className="text-xl text-center pt-2 pb-2" style={styles.background2}>
                         How To Play
                     </div>
                     <div className="text-lg pl-10 h-80" style={styles.background1}>
